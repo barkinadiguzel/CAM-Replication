f038aa22ad0f9129c39946e6817dc4497bc4475e
barkin <63337752+barkinadiguzel@users.noreply.github.com>
2026-03-28 12:04:47 +0300

    Update classifier_head.py

--- src/head/classifier_head.py
+++ src/head/classifier_head.py
@@ -5,5 +5,9 @@ class ClassifierHead(nn.Module):
     def __init__(self, in_features, num_classes):
         super().__init__()
         self.fc = nn.Linear(in_features, num_classes)
+        self.softmax = nn.Softmax(dim=1)  
+
     def forward(self, x):
-        return self.fc(x)
+        x = self.fc(x)      
+        x = self.softmax(x) 
+        return x
