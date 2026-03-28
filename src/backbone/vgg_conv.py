8b313b6d3b392156619af35448093676f7a829b9
barkin <63337752+barkinadiguzel@users.noreply.github.com>
2026-03-28 12:01:12 +0300

    Create vgg_conv.py

---
+++ src/backbone/vgg_conv.py
@@ -0,0 +1,27 @@
+import torch
+import torch.nn as nn
+
+class VGGConv(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 64, 3, padding=1), nn.ReLU(inplace=True),
+            nn.Conv2d(64, 64, 3, padding=1), nn.ReLU(inplace=True),
+            nn.MaxPool2d(2,2),
+            nn.Conv2d(64, 128, 3, padding=1), nn.ReLU(inplace=True),
+            nn.Conv2d(128, 128, 3, padding=1), nn.ReLU(inplace=True),
+            nn.MaxPool2d(2,2),
+            nn.Conv2d(128, 256, 3, padding=1), nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, 3, padding=1), nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, 3, padding=1), nn.ReLU(inplace=True),
+            nn.MaxPool2d(2,2),
+            nn.Conv2d(256, 512, 3, padding=1), nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, 3, padding=1), nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, 3, padding=1), nn.ReLU(inplace=True),
+            nn.MaxPool2d(2,2),
+            nn.Conv2d(512, 512, 3, padding=1), nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, 3, padding=1), nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, 3, padding=1), nn.ReLU(inplace=True)
+        )
+    def forward(self, x):
+        return self.features(x)
