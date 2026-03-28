6a92dbff69aff93487ba2b81e3c27b6acad8dbc7
barkin <63337752+barkinadiguzel@users.noreply.github.com>
2026-03-28 12:00:40 +0300

    Create alexnet_conv.py

---
+++ src/backbone/alexnet_conv.py
@@ -0,0 +1,26 @@
+import torch
+import torch.nn as nn
+
+class AlexNetConv(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            
+            nn.Conv2d(64, 192, kernel_size=5, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            
+            nn.Conv2d(192, 384, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            
+            nn.Conv2d(384, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True)
+        )
+    def forward(self, x):
+        return self.features(x)
